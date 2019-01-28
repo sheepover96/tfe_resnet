fca46cb7602c2ca1a7f00932e63c34b0a1d38b8d
Maiki Okura <sheep.over@gmail.com>
2019-01-28 22:28:12 +0900

    tmp

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ def main():
     def grad(model, inputs, targets):
         with tf.GradientTape() as tape:
             loss_value = loss(model, inputs, targets)
-        return tape.gradient(loss_value, model.trainable_variables)
+        return tape.gradient(loss_value, model.variables)
 
     (x_train, y_train), (x_test, y_test) = tfk.datasets.cifar100.load_data()
 
@@ -25,28 +25,23 @@ def main():
        tf.cast(y_train,tf.int64)))
     dataset = dataset.shuffle(100).batch(32)
 
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.00001)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
     model = resnet18()
 
-    for epoch in range(1):
+    for epoch in range(1000):
         epoch_accuracy = tfe.metrics.Accuracy()
-        for (batch, (images, labels)) in enumerate(dataset.take(400)):
+        for (batch, (images, labels)) in enumerate(dataset.take(128)):
             grads = grad(model, images, labels)
-            optimizer.apply_gradients(zip(grads, model.variables), global_step=tf.train.get_or_create_global_step())
+            optimizer.apply_gradients(zip(grads, model.trainable_variables), global_step=tf.train.get_or_create_global_step())
             epoch_accuracy(tf.argmax(model(images), axis=1, output_type=tf.int64), labels[:,0])
-            print(epoch_accuracy.result())
+        print(epoch_accuracy.result())
 
 
     test_res = model(tf.cast(x_test/255.0, tf.float32))
     y_test = tf.convert_to_tensor(y_test, tf.int32)
-    print(y_test.shape)
-
-    print(tf.argmax(test_res, axis=1, output_type=tf.int32).shape)
     prec = tf.equal(tf.argmax(test_res, axis=1, output_type=tf.int32), y_test[:,0])
-    print(type(prec))
-    print(prec.shape)
     train_acc = tf.reduce_mean(tf.cast(prec, tf.float32))
-    printtrain_acc.)
+    print(train_acc)
 
 if __name__ == '__main__':
     main()
