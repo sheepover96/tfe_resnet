651652db3e083b6fe3516f015e07722abf81d5dc
sheepover96 <sheep.over@gmail.com>
2019-01-28 16:15:54 +0900

    accuracy

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import pprint
 tf.enable_eager_execution()
 tfk = tf.keras
+tfe = tf.contrib.eager
 
 from resnet_model import resnet18, resnet34, CNN
 
@@ -27,15 +28,25 @@ def main():
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.00001)
     model = resnet18()
 
-    for epoch in range(2):
+    for epoch in range(1):
+        epoch_accuracy = tfe.metrics.Accuracy()
         for (batch, (images, labels)) in enumerate(dataset.take(400)):
             grads = grad(model, images, labels)
             optimizer.apply_gradients(zip(grads, model.variables), global_step=tf.train.get_or_create_global_step())
+            epoch_accuracy(tf.argmax(model(images), axis=1, output_type=tf.int64), labels[:,0])
+            print(epoch_accuracy.result())
+
 
     test_res = model(tf.cast(x_test/255.0, tf.float32))
-    prec = tf.equal(tf.argmax(test_res, axis=1, output_type=tf.int32), y_test)
+    y_test = tf.convert_to_tensor(y_test, tf.int32)
+    print(y_test.shape)
+
+    print(tf.argmax(test_res, axis=1, output_type=tf.int32).shape)
+    prec = tf.equal(tf.argmax(test_res, axis=1, output_type=tf.int32), y_test[:,0])
+    print(type(prec))
+    print(prec.shape)
     train_acc = tf.reduce_mean(tf.cast(prec, tf.float32))
-    print(train_acc)
+    printtrain_acc.)
 
 if __name__ == '__main__':
     main()
